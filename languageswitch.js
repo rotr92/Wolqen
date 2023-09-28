0c9d2bf150a62403a9604847756f046fc9cd3483
Ronny <ronny@angelaq.de>
2023-09-28 10:28:59 +0200

    Progress

---
+++ languageswitch.js
@@ -0,0 +1,7 @@
+function changeLanguage(language) {
+    if (language === 'de') {
+        window.location.href = 'index_de.html'; // Verweise auf die deutsche Version
+    } else if (language === 'en') {
+        window.location.href = 'index_en.html'; // Verweise auf die englische Version
+    }
+}
